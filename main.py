49e130c192dc5be263e453999f5d04dd4b07be25
ExhaustedDev <76536523+ExhaustedDev@users.noreply.github.com>
2021-06-21 20:20:31 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,28 @@
 import scanner
 
 # TODO: 1. Open and Read file
-scan_obj = scanner.Scanner()
+def main():
+  scan_obj = scanner.Scanner()
 
-fileIn = open("BASICcode.txt", "r")
-basic_code = fileIn.read()
-words = basic_code.split()
-  
-# Big part after this print statement where we will validate tokens and count.
+  fileIn = open("BASICcode.txt", "r")
+  basic_code = fileIn.read()
+  words = basic_code.split()
+    
+  # Big part after this print statement where we will validate tokens and count.
 
-# TODO: 2. Write to existing output file
+  # TODO: 2. Write to existing output file
 
-# After counting we then overwrite the empty output file with a 'count' or whatever we decide to use.
+  # After counting we then overwrite the empty output file with a 'count' or whatever we decide to use.
 
-fileOut = open("output.txt", "w")
-# Will need a format for the information we will write to the file.
-for word in words:
-  token = scan_obj.lookup(word)
-  if bool(token):
-    fileOut.write(token)
+  fileOut = open("output.txt", "w")
+  # Will need a format for the information we will write to the file.
+  for word in words:
+    token = scan_obj.lookup(word)
+    if bool(token):
+      fileOut.write(str(token))
+      fileOut.write(" ")
+      #print("wrote " + str(token) + " to file")
+  fileOut.close()
 
-
-print(fileOut.read())
+if __name__ == "__main__":
+    main()
