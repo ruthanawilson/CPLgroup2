67327406c4bdc307c20f5df6da2373163b17703e
ExhaustedDev <76536523+ExhaustedDev@users.noreply.github.com>
2021-07-06 00:06:42 -0400

    Update main.py
    Changed main file's interaction with scanner, due to scanner handling input file directly

--- main.py
+++ main.py
@@ -8,26 +8,24 @@ import scanner
 
 # TODO: 1. Open and Read file
 def main():
-  scan_obj = scanner.Scanner()
-
-  fileIn = open("BASICcode.txt", "r")
-  basic_code = fileIn.read()
-  words = basic_code.split()
+  #scanner now handles file input directly through get_char function
+  basic_code = "BASICcode.txt"
+  scan_obj = scanner.Scanner(basic_code)
     
   # Big part after this print statement where we will validate tokens and count.
 
   # TODO: 2. Write to existing output file
 
   # After counting we then overwrite the empty output file with a 'count' or whatever we decide to use.
-
+  
   fileOut = open("output.txt", "w")
   # Will need a format for the information we will write to the file.
-  for word in words:
-    token = scan_obj.lookup(word)
-    if bool(token):
-      fileOut.write(str(token))
-      fileOut.write(" ")
-      #print("wrote " + str(token) + " to file")
+  
+  while scan_obj.next_token != 900
+    scan_obj.get_token()
+    token = scan_obj.next_token
+    fileOut.write(str(token))
+    fileOut.write("\n")        #print("wrote " + str(token) + " to file")
   fileOut.close()
 
 if __name__ == "__main__":
