7a37414039ea4d0343199dc0c891514a331a36b4
ExhaustedDev <76536523+ExhaustedDev@users.noreply.github.com>
2021-07-05 23:56:05 -0400

    Update scanner.py
    Changing from word based to character based, lets us handle comments, separate tokens that don't use spaces

--- scanner.py
+++ scanner.py
@@ -24,33 +24,102 @@ class Scanner:
             "STEP"  : 140,
         #"single letter (variable)"  : "150",  special case so handled seperately
         #  "number"  : 160,                    special case handled seperately
-            "END"  : 170,
-            ";"  : 180,
-            "$"  : 190,
-            "="  : 200,
-            ">"  : 210,
-            "<"  : 220,
-            ":"  : 230,   }
-
-    #this expexts to be given each word after the input is divided by whitespace
-    def lookup(self, lexeme):  
-        vals = self.check_keywords(lexeme)
-        if vals[0]:
-            self.count += 1
-            return vals[1]
-        elif len(lexeme) == 1 and lexeme.isalpha():
-            self.count += 1
-            return 150
-        elif lexeme.isdigit():
-            self.count += 1
-            return 160
+        #  string literal : 170                special case 
+            "END"  : 180,
+            "LET"  : 190, 
+        }
+        self.ops = {
+            "+"  : 200,
+            "-"  : 210,
+            "*"  : 220,
+            "/"  : 230,
+            "("  : 240,
+            ")"  : 250,  
+            "<"  : 260,
+            ">"  : 270,
+            "="  : 280,
+            ";"  : 290,
+            ":"  : 300,
+            "$"  : 310, 
+            }
+    #gets the next character from the file, sets the character type 
+    def get_char(self):
+        self.next_char = self.fileIn.read(1)
+        if self.next_char.isalpha():
+            self.char_type = 0
+        elif self.next_char.isdigit():
+            self.char_type = 1
+        elif self.next_char == "":
+            self.char_type = 900
         else:
-            #if we dont have an identifier by now then somethings wrong
+            self.char_type = 99
+
+    #adds the current character in next_char into the current lexeme being built
+    def add_char(self):
+        self.lexeme = self.lexeme + self.next_char
+
+    def get_token(self):
+        self.lexeme = ""
+        self.skip_whitespace()
+        if self.char_type == 0:
+            self.handle_names()          #names and keywords are treated the same, 
+        elif self.char_type == 1:        #except a check is made to see if the name is in keyword dict
+            self.handle_ints()
+        elif self.char_type == 99:
+            self.check_op(self.next_char)
+        elif self.char_type == 900:
+            self.next_token = 900
+
+    def handle_names(self):
+        self.add_char()
+        self.get_char()
+        self.get_lexeme([0,1])
+        if self.lexeme =="REM":
+                self.skip_line()
+                self.get_token()
+        elif self.lexeme in self.keywords.keys():
+            self.next_token = self.keywords[self.lexeme]
+        else:
+            self.next_token = 150
+
+    def handle_ints(self):
+        self.add_char()
+        self.get_char()
+        self.get_lexeme([1])
+        self.next_token = 160
+
+    def skip_whitespace(self):
+        if self.char_type == 900:
             return
+        while self.next_char in string.whitespace:
+            self.get_char()
 
-    def check_keywords(self, lexeme):
-        if lexeme not in self.tokens.keys():
-            return [False, None]
-        else:
-            token = self.tokens[lexeme]
-            return [True, token]
+    #currently only used for comments, continues 
+    #to grab characters until one after a \n
+    def skip_line(self):
+        while self.next_char != '\n':
+            self.get_char()
+        self.get_char()
+
+    def check_op(self, c):
+        if c in self.ops.keys():
+                self.add_char()
+                self.next_token = self.ops[self.next_char]
+                self.get_char()
+        elif c == '"':
+            self.add_char()
+            self.get_char()
+            while self.next_char != '"':
+                self.add_char()
+                self.get_char()
+            self.add_char()
+            self.get_char()
+            self.next_token = 300
+        
+            
+
+    #continues to get characters if they are in condition list 
+    def get_lexeme(self, conds):
+        while self.char_type in conds:
+            self.add_char()
+            self.get_char()
